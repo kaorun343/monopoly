61dfdb405ff42353ee709e13cbcc65427792653f
kaorun343 <hagiharak4294967296@gmail.com>
2019-03-07 03:53:47 +0900

    Create Jail.ts

---
+++ src/models/Jail.spec.ts
@@ -0,0 +1,26 @@
+import { initJail, imprisonPrisoner, releasePrisoner } from './Jail'
+
+describe('Jail.ts', () => {
+  describe('initJail()', () => {
+    it('should init a Jail object', () => {
+      const jail = initJail(0, 1, 2, 3)
+      expect(jail.size).toBe(4)
+    })
+  })
+
+  describe('imprisonPrisoner()', () => {
+    it('should imprison a prisoner', () => {
+      const jail = initJail(0)
+      expect(imprisonPrisoner(jail, 30).has(30)).toBeTruthy()
+      expect(jail.has(30)).toBeFalsy()
+    })
+  })
+
+  describe('releasePrisoner()', () => {
+    it('should release a prisoner', () => {
+      const jail = initJail(0)
+      expect(releasePrisoner(jail, 0).has(0)).toBeFalsy()
+      expect(jail.has(0)).toBeTruthy()
+    })
+  })
+})
---
+++ src/models/Jail.ts
@@ -0,0 +1,31 @@
+import { Player } from './Player'
+
+export type Jail<P = Player> = ReadonlyMap<P, number>
+
+/**
+ * init a jail object
+ * @param prisoners the prisoners to be imprisoned
+ */
+export function initJail<P>(...prisoners: P[]): Jail<P> {
+  return new Map(prisoners.map(prisoner => [prisoner, 0] as [P, number]))
+}
+
+/**
+ * imprison the prisoner to the jail
+ * @param jail the target jail object
+ * @param prisoner to be imprisoned
+ */
+export function imprisonPrisoner<P>(jail: Jail<P>, prisoner: P): Jail<P> {
+  return new Map(jail).set(prisoner, 0)
+}
+
+/**
+ * release the prisoner form the jail
+ * @param jail the target jail object
+ * @param prisoner to be released
+ */
+export function releasePrisoner<P>(jail: Jail<P>, prisoner: P): Jail<P> {
+  const newJail = new Map(jail)
+  newJail.delete(prisoner)
+  return newJail
+}
