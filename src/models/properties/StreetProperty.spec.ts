00aab6818d849f6085291ea984c7ae20d0a231ea
kaorun343 <hagiharak4294967296@gmail.com>
2019-03-13 02:31:12 +0900

    Update StreetProperty.ts

---
+++ src/models/properties/StreetProperty.spec.ts
@@ -0,0 +1,39 @@
+import {
+  StreetColor,
+  initStreetProperty,
+  calculateStreetAmount,
+} from './StreetProperty'
+
+describe('calculateStreetAmount()', () => {
+  const info = new Map([[StreetColor.DarkBlue, 2], [StreetColor.LightBlue, 3]])
+
+  it('should calculate the rental money when not all streets of a specific color are owned', () => {
+    const ownedDarkBlue = [initStreetProperty(StreetColor.DarkBlue, 35)]
+    const [parkPlace] = ownedDarkBlue
+    expect(calculateStreetAmount(ownedDarkBlue, parkPlace, info)).toBe(35)
+
+    const ownedLightBlue = [
+      initStreetProperty(StreetColor.LightBlue, 6),
+      initStreetProperty(StreetColor.LightBlue, 6),
+    ]
+    const orientalAvenue = ownedLightBlue[0]
+    expect(calculateStreetAmount(ownedLightBlue, orientalAvenue, info)).toBe(6)
+  })
+
+  it('should calculate the rental money when all streets of a specific colore are owned', () => {
+    const ownedDarkBlue = [
+      initStreetProperty(StreetColor.DarkBlue, 35),
+      initStreetProperty(StreetColor.DarkBlue, 50),
+    ]
+    const parkPlace = ownedDarkBlue[0]
+    expect(calculateStreetAmount(ownedDarkBlue, parkPlace, info)).toBe(70)
+
+    const ownedLightBlue = [
+      initStreetProperty(StreetColor.LightBlue, 6),
+      initStreetProperty(StreetColor.LightBlue, 6),
+      initStreetProperty(StreetColor.LightBlue, 8),
+    ]
+    const orientalAvenue = ownedLightBlue[0]
+    expect(calculateStreetAmount(ownedLightBlue, orientalAvenue, info)).toBe(12)
+  })
+})
--- src/models/properties/StreetProperty.ts
+++ src/models/properties/StreetProperty.ts
@@ -11,7 +11,26 @@ export const enum StreetColor {
   DarkBlue,
 }
 
+export type StreetColorInfo = ReadonlyMap<StreetColor, number>
+
 export interface StreetProperty {
   readonly type: PropertyType.Street
   readonly color: StreetColor
+  readonly rent: number
+}
+
+export function initStreetProperty(
+  color: StreetColor,
+  rent: number,
+): StreetProperty {
+  return { type: PropertyType.Street, color, rent }
+}
+
+export function calculateStreetAmount(
+  properties: ReadonlyArray<StreetProperty>,
+  property: Readonly<StreetProperty>,
+  info: StreetColorInfo,
+) {
+  const rent = property.rent
+  return properties.length < info.get(property.color)! ? rent : rent * 2
 }
