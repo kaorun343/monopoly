70500ef1de704ea7ee5b2c9362c3f9bb667ed7ee
kaorun343 <hagiharak4294967296@gmail.com>
2019-03-14 02:20:04 +0900

    Add bankrupt property into Player

--- src/models/Player.spec.ts
+++ src/models/Player.spec.ts
@@ -1,18 +1,16 @@
-import { Player, recieveMoney, payMoney } from './Player'
+import { Player, recieveMoney, payMoney, bankrupt } from './Player'
 import { Right, Left } from './utils/Either'
+
+const player = Player('', 1500)
 
 describe('recieveMoney()', () => {
   it('should update money property', () => {
-    const player = Player('', 1500)
-
     expect(recieveMoney(player, 200).money).toBe(1700)
     expect(player.money).toBe(1500)
   })
 })
 
 describe('payMoney()', () => {
-  const player = Player('', 1500)
-
   it('should return Right when the player can pay', () => {
     expect(payMoney(player, 1200)).toEqual(Right(Player('', 300)))
     expect(payMoney(player, 1500)).toEqual(Right(Player('', 0)))
@@ -21,5 +19,11 @@ describe('payMoney()', () => {
 
   it('should return Left when the player cannot pay', () => {
     expect(payMoney(player, 1501)).toEqual(Left(null))
+  })
+})
+
+describe('bankrupt()', () => {
+  it('should update the state', () => {
+    expect(bankrupt(player).bankrupt).toBe(true)
   })
 })
--- src/models/Player.ts
+++ src/models/Player.ts
@@ -3,11 +3,12 @@ import { Either, Right, Left } from './utils/Either'
 export type Player = {
   readonly name: string
   readonly money: number
+  readonly bankrupt: boolean
   readonly arrested: boolean
 }
 
 export function Player(name: string, money: number): Player {
-  return { name, money, arrested: false }
+  return { name, money, bankrupt: false, arrested: false }
 }
 
 export function recieveMoney(player: Player, money: number): Player {
@@ -17,4 +18,8 @@ export function recieveMoney(player: Player, money: number): Player {
 export function payMoney(player: Player, money: number): Either<Player, null> {
   const result = player.money - money
   return result >= 0 ? Right({ ...player, money: result }) : Left(null)
+}
+
+export function bankrupt(player: Player): Player {
+  return { ...player, bankrupt: true }
 }
