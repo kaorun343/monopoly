1f8ccde6d4f0f256eb01211c1f50d2ecb51eefce
kaorun343 <hagiharak4294967296@gmail.com>
2019-03-26 06:29:14 +0900

    Update PlayerPosition

--- src/models/player/PlayerPosition.spec.ts
+++ src/models/player/PlayerPosition.spec.ts
@@ -1,4 +1,4 @@
-import { addDice, PlayerPosition } from './PlayerPosition'
+import { addDice, PlayerPosition, moveToDestination } from './PlayerPosition'
 import { Dice } from '../Dice'
 import { PROPERTY_AMOUNT } from '../../constants'
 
@@ -20,6 +20,32 @@ describe(addDice, () => {
     it('should return a new position', () => {
       const start = PROPERTY_AMOUNT - 10 + 2
       expect(addDice(PlayerPosition(start), Dice(5, 5))).toBe(PlayerPosition(2))
+    })
+  })
+})
+
+describe(moveToDestination, () => {
+  describe('when destination is before GO', () => {
+    it('should return routes', () => {
+      const origin = PlayerPosition(1)
+      const destination = PlayerPosition(5)
+      expect(moveToDestination(origin, destination)).toEqual([2, 3, 4, 5])
+    })
+  })
+
+  describe('when destination is GO', () => {
+    it('should return routes', () => {
+      const origin = PlayerPosition(38)
+      const destination = PlayerPosition(0)
+      expect(moveToDestination(origin, destination)).toEqual([39, 0])
+    })
+  })
+
+  describe('when destination is after GO', () => {
+    it('should return routes', () => {
+      const origin = PlayerPosition(38)
+      const destination = PlayerPosition(2)
+      expect(moveToDestination(origin, destination)).toEqual([39, 0, 1, 2])
     })
   })
 })
--- src/models/player/PlayerPosition.ts
+++ src/models/player/PlayerPosition.ts
@@ -7,10 +7,27 @@ export function PlayerPosition(position: number) {
   return position as PlayerPosition
 }
 
+function mod(position: number) {
+  return position % PROPERTY_AMOUNT
+}
+
 export function addDice(position: PlayerPosition, dice: Dice) {
   const destination = (position as number) + toPrimitiveValue(dice)
 
-  return destination >= PROPERTY_AMOUNT
-    ? ((destination - PROPERTY_AMOUNT) as PlayerPosition)
-    : (destination as PlayerPosition)
+  return mod(destination) as PlayerPosition
+}
+
+export function moveToDestination(
+  origin: PlayerPosition,
+  destination: PlayerPosition,
+): PlayerPosition[] {
+  const result = []
+  let position = origin as number
+
+  while (position !== (destination as number)) {
+    position = mod(position + 1)
+    result.push(position as PlayerPosition)
+  }
+
+  return result
 }
